af781efb72a00f578929f094da74d70ac3bc8683
Dedi setiawan halawa <dedihalawa@Hello-Dedi.local>
2026-09-21 16:49:13 +0700

    chore: backup current library dashboard

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Perpustakaan Digital UMA",
+  title: "USEFECT",
   description:
     "Perpustakaan Digital Universitas Medan Area - Satu Akses Untuk Semua Ilmu",
 };
