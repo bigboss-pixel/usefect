e08e4536f2290b6013ebf03686a3605789c01a6d
Dedi setiawan halawa <dedihalawa@Hello-Dedi.local>
2026-09-25 17:02:57 +0700

    fix: respect platform assigned port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = Number(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT ?? 3001);
   await app.listen(port, '0.0.0.0');
 }
 
